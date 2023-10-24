d7c83a084f4a5d79c6909c329315e8f70b331788
huu trinh <huutrinh1310@gmail.com>
2023-10-24 10:04:34 +0700

    ui: button component

--- src/components/shared/button/index.tsx
+++ src/components/shared/button/index.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import "./index.module.scss";
+import styles from "./index.module.scss";
 
 export interface IButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  primary?: boolean;
+  secondary?: boolean;
+  tertiary?: boolean;
+  sm?: boolean;
+  md?: boolean;
+  lg?: boolean;
+}
 
 const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, primary, secondary, tertiary, sm, md, lg, ...props }, ref) => {
+    const classnames = [
+      styles["btn"],
+      styles["btn-normal"],
+      className,
+      primary && styles["btn-main"],
+      secondary && styles["btn-secondary"],
+      tertiary && styles["btn-tertiary"],
+      sm && styles["btn-small"],
+      md && styles["btn-medium"],
+      lg && styles["btn-normal"],
+    ].join(" ");
+
     return (
-      <button className={className} ref={ref} {...props}>
+      <button className={classnames} ref={ref} {...props}>
         {props.children}
       </button>
     );
