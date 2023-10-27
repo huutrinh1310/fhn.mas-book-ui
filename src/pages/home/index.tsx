2d2b9993d5a1b8e34a0905434a9fe68bd34ca439
huu trinh <huutrinh1310@gmail.com>
2023-10-27 10:43:45 +0700

    config: pages to route

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface IHomeProps {}
+
+export default function HomePage(props: IHomeProps) {
+  return <div>This is Home</div>;
+}
---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface ISettingPageProps {}
+
+export default function SettingPage(props: ISettingPageProps) {
+  return <div>Hello Setting Page day</div>;
+}
